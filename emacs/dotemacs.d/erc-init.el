cbe69dda53ef50b9cbb4371e8496d7efa4eba32f
tingle2008 <tingle2008@localhost>
2011-09-19 18:09:05 +0800

    erc el add log but failed

--- emacs/dotemacs.d/erc-init.el
+++ emacs/dotemacs.d/erc-init.el
@@ -1,4 +1,11 @@
 (require 'erc)
+(setq erc-log-channels-directory "~/.erc/logs/")
+(setq erc-save-buffer-on-part t)
+(setq erc-log-insert-log-on-open nil)
+(setq erc-hide-timestamps t)
+(defadvice save-buffers-kill-emacs (before save-logs (arg) activate)
+(save-some-buffers t (lambda () (when (eq major-mode 'erc-mode) t))))
+
 (defun myerc-join-channels (&rest channels)
   (mapc 'erc-join-channel channels))
 (defun myerc-autojoin-channels (server nick)
