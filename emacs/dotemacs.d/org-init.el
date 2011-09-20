8f6a87e61e020d0d3f4c03edd5f53f34cfe8e862
tingle2008 <tingle2008@localhost>
2011-09-20 17:45:42 +0800

    org-mode add agenda

--- emacs/dotemacs.d/org-init.el
+++ emacs/dotemacs.d/org-init.el
@@ -1,10 +1,100 @@
 (provide 'org-init)
 
 (setq load-path (add-to-list 'load-path "~/.emacs.d/org-mode/lisp"))
+(add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
 (require 'org-install)
-(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+
+
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
+(define-key global-map "\C-cb" 'org-iswitchb)
+
+;; ;; orgstruct++-mode is enabled in Gnus message buffers to aid in creating structured email messages.
+;; (add-hook 'message-mode-hook 'orgstruct++-mode 'append)
+;; (add-hook 'message-mode-hook 'turn-on-auto-fill 'append)
+;; (add-hook 'message-mode-hook 'bbdb-define-all-aliases 'append)
+;; (add-hook 'message-mode-hook 'orgtbl-mode 'append)
+;; (add-hook 'message-mode-hook 'turn-on-flyspell 'append)
+;; (add-hook 'message-mode-hook '(lambda () (setq fill-column 72)) 'append)
+;; (add-hook 'message-mode-hook '(lambda () (local-set-key (kbd "C-c M-o") 'org-mime-htmlize)) 'append)
+
+
+;; Custom Key Bindings
+(global-set-key (kbd "<f12>") 'org-agenda)
+(global-set-key (kbd "<f5>") 'bh/org-todo)
+(global-set-key (kbd "<S-f5>") 'bh/widen)
+(global-set-key (kbd "<f7>") 'bh/set-truncate-lines)
+(global-set-key (kbd "<f8>") 'org-cycle-agenda-files)
+(global-set-key (kbd "<f9> b") 'bbdb)
+(global-set-key (kbd "<f9> c") 'calendar)
+(global-set-key (kbd "<f9> f") 'boxquote-insert-file)
+(global-set-key (kbd "<f9> g") 'gnus)
+(global-set-key (kbd "<f9> h") 'bh/hide-other)
+(global-set-key (kbd "<f9> i") 'info)
+(global-set-key (kbd "<f9> I") 'bh/punch-in)
+(global-set-key (kbd "<f9> O") 'bh/punch-out)
+(global-set-key (kbd "<f9> o") 'bh/make-org-scratch)
+(global-set-key (kbd "<f9> r") 'boxquote-region)
+(global-set-key (kbd "<f9> s") 'bh/switch-to-scratch)
+
+(global-set-key (kbd "<f9> t") 'bh/insert-inactive-timestamp)
+(global-set-key (kbd "<f9> u") 'bh/untabify)
+
+(global-set-key (kbd "<f9> v") 'visible-mode)
+(global-set-key (kbd "<f9> SPC") 'bh/clock-in-last-task)
+(global-set-key (kbd "C-<f9>") 'previous-buffer)
+(global-set-key (kbd "C-x n r") 'narrow-to-region)
+(global-set-key (kbd "C-<f10>") 'next-buffer)
+(global-set-key (kbd "<f11>") 'org-clock-goto)
+(global-set-key (kbd "C-<f11>") 'org-clock-in)
+(global-set-key (kbd "C-s-<f12>") 'bh/save-then-publish)
+(global-set-key (kbd "C-M-r") 'org-capture)
+(global-set-key (kbd "C-c r") 'org-capture)
+
+(defun bh/hide-other ()
+  (interactive)
+  (save-excursion
+    (org-back-to-heading)
+    (org-shifttab)
+    (org-reveal)
+    (org-cycle)))
+
+(defun bh/set-truncate-lines ()
+  "Toggle value of truncate-lines and refresh window display."
+  (interactive)
+  (setq truncate-lines (not truncate-lines))
+  ;; now refresh window display (an idiom from simple.el):
+  (save-excursion
+    (set-window-start (selected-window)
+                      (window-start (selected-window)))))
+
+(defun bh/make-org-scratch ()
+  (interactive)
+  (find-file "/tmp/publish/scratch.org")
+  (gnus-make-directory "/tmp/publish"))
+
+(defun bh/switch-to-scratch ()
+  (interactive)
+  (switch-to-buffer "*scratch*"))
+
+(defun bh/untabify ()
+  (interactive)
+  (untabify (point-min) (point-max)))
+
+
+(setq org-todo-state-tags-triggers
+      (quote (("CANCELLED" ("CANCELLED" . t))
+              ("WAITING" ("WAITING" . t))
+              ("SOMEDAY" ("WAITING" . t))
+              (done ("WAITING"))
+              ("TODO" ("WAITING") ("CANCELLED"))
+              ("NEXT" ("WAITING"))
+              ("STARTED" ("WAITING"))
+              ("DONE" ("WAITING") ("CANCELLED")))))
+
+
+(setq org-treat-S-cursor-todo-selection-as-state-change nil)
+
 (setq org-log-done t)
 (setq org-fontify-emphasized-text t
       org-descriptive-links nil
@@ -23,23 +113,79 @@
       )
 
 
-(setq org-agenda-files (list "~/org/todolist.org"
-			     "~/org/memo-tasks.org"
-			     "~/org/someday.org"
+(setq org-agenda-files (list "~/org/"
 			     ))
 (add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\)$" . org-mode))
 
 (setq org-todo-keywords
-      '((sequence "TODO(t)" "WAITING(w)" "NOW(n)" "|" "DONE(d)" "ABORT(a)")))
+      (quote ((sequence "TODO(t)" "NEXT(n)" "STARTED(s)" "|" "DONE(d!/!)")
+              (sequence "WAITING(w@/!)" "SOMEDAY(S!)" "|" "CANCELLED(c@/!)" "PHONE")
+              (sequence "OPEN(O!)" "|" "CLOSED(C!)"))))
 
-;; Change task state to STARTED when clocking in
-(setq org-clock-in-switch-to-state "NOW")
+(setq org-todo-keyword-faces 
+      (quote (("TODO" :foreground "red" :weight bold)
+              ("NEXT" :foreground "blue" :weight bold)
+              ("STARTED" :foreground "blue" :weight bold)
+              ("DONE" :foreground "forest green" :weight bold)
+              ("WAITING" :foreground "orange" :weight bold)
+              ("SOMEDAY" :foreground "magenta" :weight bold)
+              ("CANCELLED" :foreground "forest green" :weight bold)
+              ("OPEN" :foreground "blue" :weight bold)
+              ("CLOSED" :foreground "forest green" :weight bold)
+              ("PHONE" :foreground "forest green" :weight bold))))
 
-(setq org-todo-keyword-faces
-      (quote (("TODO" :background "blue" :weight bold)
-	      ("WAITING" :background "green" :foreground "blue" :weight bold)
-	      ("NOW" :background "red" :weight bold)
-	      )))
+
+(setq org-default-notes-file "~/org/refile.org")
+
+;; I use C-M-r to start capture mode
+(global-set-key (kbd "C-M-r") 'org-capture)
+;; I use C-c r to start capture mode when using SSH from my Android phone
+;; (global-set-key (kbd "C-c r") 'org-capture)
+
+;; Capture templates for: TODO tasks, Notes, appointments, phone calls, and org-protocol
+(setq org-capture-templates
+      (quote (("t" "todo" entry (file "~/org/refile.org")
+               "* TODO %?\n%U\n%a\n  %i" :clock-in t :clock-resume t)
+              ("n" "note" entry (file "~/org/refile.org")
+               "* %? :NOTE:\n%U\n%a\n  %i" :clock-in t :clock-resume t)
+              ("j" "Journal" entry (file+datetree "~/org/diary.org")
+               "* %?\n%U\n  %i" :clock-in t :clock-resume t)
+              ("w" "org-protocol" entry (file "~/org/refile.org")
+               "* TODO Review %c\n%U\n  %i" :immediate-finish t)
+              ("p" "Phone call" entry (file "~/org/refile.org")
+               "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
+              ("h" "Habit" entry (file "~/org/refile.org")
+               "* NEXT %?\n%U\n%a\nSCHEDULED: %t .+1d/3d\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n  %i"))))
+
+
+
+; Targets include this file and any file contributing to the agenda - up to 2 levels deep
+(setq org-refile-targets (quote ((nil :maxlevel . 3)
+                                 (org-agenda-files :maxlevel . 3))))
+
+; Stop using paths for refile targets - we file directly with IDO
+(setq org-refile-use-outline-path nil)
+
+; Targets complete directly with IDO
+(setq org-outline-path-complete-in-steps nil)
+
+; Allow refile to create parent tasks with confirmation
+(setq org-refile-allow-creating-parent-nodes (quote confirm))
+
+; Use IDO for both buffer and file completion and ido-everywhere to t
+(setq org-completion-use-ido t)
+(setq ido-everywhere t)
+(setq ido-max-directory-size 100000)
+(ido-mode (quote both))
+
+;;;; Refile settings
+; Exclude DONE state tasks from refile targets
+(defun bh/verify-refile-target ()
+  "Exclude todo keywords with a done state from refile targets"
+  (not (member (nth 2 (org-heading-components)) org-done-keywords)))
+
+(setq org-refile-target-verify-function 'bh/verify-refile-target)
+
 
 ;; Resume clocking tasks when emacs is restarted
 (setq org-clock-persistence-insinuate)
@@ -50,44 +196,109 @@
 ;; Save the running clock and all clock history when exiting Emacs, load it on startup
 (setq org-clock-persist t)
 
-(setq org-tag-alist '(
+
+(setq org-tag-alist (quote ((:startgroup)
+		      ("@errand" . ?e)
+		      ("@office" . ?o)
+		      ("@home" . ?h)
+		      ("@farm" . ?f)
+		      (:endgroup)
 		      ("Project" . ?p)
-		      ("Writing" . ?w)
-		      ("Conference" . ?c)
-		      ("Lecture" . ?l)
 		      ("Reading" . ?r)
 		      ("Tasks" . ?t)
 		      ("Misc" . ?m)
-		      ))
+		      ("WAITING" . ?w)
+		      ("CANCELLED" . ?c)
+		      ("NOTES" . ?n))))
 
+;;15.11 Visiting Links XXX: vm stucked
+;;(setq org-link-frame-setup ((vm . vm-visit-folder)
+;;                            (gnus . org-gnus-no-new-news)
+;;                            (file . find-file-other-window)))
+
+;; Do not dim blocked tasks
+(setq org-agenda-dim-blocked-tasks nil)
+
+;; Custom agenda command definitions
 (setq org-agenda-custom-commands
-      '(
-	("P" "Projects"
-	 ((tags-todo "Project")
-	  (tags-todo "Writing")
-	  (tags-todo "Tasks")))
-	("c" "DOING"
-	 ((todo "NOW")))
-	("h" "Office and Home Lists"
-	 (;(agenda)
-	  (tags-todo "Project")
-	  (tags-todo "Writing")
-	  (tags-todo "Conference")
-	  (tags-todo "Lecture")
-	  (tags-todo "Reading")
-	  (tags-todo "Tasks")
-	  (tags-todo "Misc")))
-	("d" "Daily Action List"
-	 ((agenda "" ((org-agenda-ndays 1)
-		      (org-agenda-sorting-strategy
-		       (quote ((agenda time-up priority-down tag-up) )))
-      ))))
-	("W" "Weekly Review"
-         ((agenda "" ((org-agenda-ndays 7)))
-          (todo "TODO")
-))
-	)
-      )
+      (quote (("N" "Notes" tags "NOTE"
+               ((org-agenda-overriding-header "Notes")
+                (org-tags-match-list-sublevels t)))
+              ("h" "Habits" tags-todo "STYLE=\"habit\""
+               ((org-agenda-overriding-header "Habits")
+                (org-agenda-sorting-strategy
+                 '(todo-state-down effort-up category-keep))))
+              (" " "Agenda"
+               ((agenda "" nil)
+                (tags "REFILE"
+                      ((org-agenda-overriding-header "Notes and Tasks to Refile")
+                       (org-agenda-overriding-header "Tasks to Refile")))
+                (tags-todo "-CANCELLED/!"
+                           ((org-agenda-overriding-header "Stuck Projects")
+                            (org-tags-match-list-sublevels 'indented)
+                            (org-agenda-skip-function 'bh/skip-non-stuck-projects)))
+                (tags-todo "-WAITING-CANCELLED/!NEXT|STARTED"
+                           ((org-agenda-overriding-header "Next Tasks")
+                            (org-agenda-skip-function 'bh/skip-projects-and-habits)
+                            (org-agenda-todo-ignore-scheduled t)
+                            (org-agenda-todo-ignore-deadlines t)
+                            (org-tags-match-list-sublevels t)
+                            (org-agenda-sorting-strategy
+                             '(todo-state-down effort-up category-keep))))
+                (tags-todo "-REFILE-CANCELLED/!-NEXT-STARTED-WAITING"
+                           ((org-agenda-overriding-header "Tasks")
+                            (org-agenda-skip-function 'bh/skip-projects-and-habits)
+                            (org-tags-match-list-sublevels 'indented)
+                            (org-agenda-todo-ignore-scheduled t)
+                            (org-agenda-todo-ignore-deadlines t)
+                            (org-agenda-sorting-strategy
+                             '(category-keep))))
+                (tags-todo "-CANCELLED/!"
+                           ((org-agenda-overriding-header "Projects")
+                            (org-agenda-skip-function 'bh/skip-non-projects)
+                            (org-tags-match-list-sublevels 'indented)
+                            (org-agenda-sorting-strategy
+                             '(category-keep))))
+                (todo "WAITING|SOMEDAY"
+                      ((org-agenda-overriding-header "Waiting and Postponed tasks")
+                       (org-agenda-skip-function 'bh/skip-projects-and-habits)))
+                (tags "-REFILE/"
+                      ((org-agenda-overriding-header "Tasks to Archive")
+                       (org-agenda-skip-function 'bh/skip-non-archivable-tasks))))
+               nil)
+              ("r" "Tasks to Refile" tags "REFILE"
+               ((org-agenda-overriding-header "Notes and Tasks to Refile")
+                (org-agenda-overriding-header "Tasks to Refile")))
+              ("#" "Stuck Projects" tags-todo "-CANCELLED/!"
+               ((org-agenda-overriding-header "Stuck Projects")
+                (org-tags-match-list-sublevels 'indented)
+                (org-agenda-skip-function 'bh/skip-non-stuck-projects)))
+              ("n" "Next Tasks" tags-todo "-WAITING-CANCELLED/!NEXT|STARTED"
+               ((org-agenda-overriding-header "Next Tasks")
+                (org-agenda-skip-function 'bh/skip-projects-and-habits)
+                (org-agenda-todo-ignore-scheduled t)
+                (org-agenda-todo-ignore-deadlines t)
+                (org-tags-match-list-sublevels t)
+                (org-agenda-sorting-strategy
+                 '(todo-state-down effort-up category-keep))))
+              ("R" "Tasks" tags-todo "-REFILE-CANCELLED/!-NEXT-STARTED-WAITING"
+               ((org-agenda-overriding-header "Tasks")
+                (org-agenda-skip-function 'bh/skip-projects-and-habits)
+                (org-tags-match-list-sublevels 'indented)
+                (org-agenda-sorting-strategy
+                 '(category-keep))))
+              ("p" "Projects" tags-todo "-CANCELLED/!"
+               ((org-agenda-overriding-header "Projects")
+                (org-agenda-skip-function 'bh/skip-non-projects)
+                (org-tags-match-list-sublevels 'indented)
+                (org-agenda-sorting-strategy
+                 '(category-keep))))
+              ("w" "Waiting Tasks" todo "WAITING|SOMEDAY"
+               ((org-agenda-overriding-header "Waiting and Postponed tasks"))
+               (org-agenda-skip-function 'bh/skip-projects-and-habits))
+              ("A" "Tasks to Archive" tags "-REFILE/"
+               ((org-agenda-overriding-header "Tasks to Archive")
+                (org-agenda-skip-function 'bh/skip-non-archivable-tasks))))))
 
 ;; Always hilight the current agenda line
 (add-hook 'org-agenda-mode-hook '(lambda () (hl-line-mode 1)))
@@ -97,6 +308,3 @@
       (quote ((agenda time-up priority-down effort-up category-up)
               (todo priority-down)
               (tags priority-down))))
-
-
-
