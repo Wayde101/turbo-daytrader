2a15741aaa2f89b1b6451fa6d41ba0b0472dc689
tingle2008 <tingle2008@localhost>
2011-09-26 16:02:30 +0800

    autocomit

--- emacs/dotemacs.d/org-init.el
+++ emacs/dotemacs.d/org-init.el
@@ -309,3 +309,129 @@
       (quote ((agenda time-up priority-down effort-up category-up)
               (todo priority-down)
               (tags priority-down))))
+
+
+;; Resume clocking task on clock-in if the clock is open
+(setq org-clock-in-resume t)
+;; Change task to STARTED when clocking in
+(setq org-clock-in-switch-to-state 'bh/clock-in-to-started)
+;; Separate drawers for clocking and logs
+(setq org-drawers (quote ("PROPERTIES" "LOGBOOK")))
+;; Save clock data and state changes and notes in the LOGBOOK drawer
+(setq org-clock-into-drawer t)
+;; Sometimes I change tasks I'm clocking quickly - this removes clocked tasks with 0:00 duration
+(setq org-clock-out-remove-zero-time-clocks t)
+;; Clock out when moving task to a done state
+(setq org-clock-out-when-done t)
+;; Save the running clock and all clock history when exiting Emacs, load it on startup
+(setq org-clock-persist (quote history))
+;; Enable auto clock resolution for finding open clocks
+(setq org-clock-auto-clock-resolution (quote when-no-clock-is-running))
+;; Include current clocking task in clock reports
+(setq org-clock-report-include-clocking-task t)
+
+(setq bh/keep-clock-running nil)
+
+(defun bh/clock-in-to-started (kw)
+  "Switch task from TODO or NEXT to STARTED when clocking in.
+Skips capture tasks."
+  (if (and (member (org-get-todo-state) (list "TODO" "NEXT"))
+           (not (and (boundp 'org-capture-mode) org-capture-mode)))
+      "STARTED"))
+
+(defun bh/find-project-task ()
+  "Move point to the parent (project) task if any"
+  (let ((parent-task (save-excursion (org-back-to-heading) (point))))
+    (while (org-up-heading-safe)
+      (when (member (nth 2 (org-heading-components)) org-todo-keywords-1)
+        (setq parent-task (point))))
+    (goto-char parent-task)
+    parent-task))
+
+(add-hook 'org-agenda-mode-hook '(lambda () (org-defkey org-agenda-mode-map "\C-c\C-x<" 'bh/set-agenda-restriction-lock) 'append))
+
+(defun bh/set-agenda-restriction-lock (arg)
+  "Set restriction lock to current subtree or file if prefix is specified"
+  (interactive "p")
+  (let* ((pom (org-get-at-bol 'org-hd-marker))
+         (tags (org-with-point-at pom (org-get-tags-at))))
+    (let ((restriction-type (if (equal arg 4) 'file 'subtree)))
+      (cond
+       ((equal major-mode 'org-agenda-mode)
+        (org-with-point-at pom
+          (org-agenda-set-restriction-lock restriction-type)))
+       ((and (equal major-mode 'org-mode) (org-before-first-heading-p))
+        (org-agenda-set-restriction-lock 'file))
+       (t
+        (org-with-point-at pom
+          (org-agenda-set-restriction-lock restriction-type)))))))
+
+(defun bh/punch-in (arg)
+  "Start continuous clocking and set the default task to the
+selected task.  If no task is selected set the Organization task
+as the default task."
+  (interactive "p")
+  (setq bh/keep-clock-running t)
+  (if (equal major-mode 'org-agenda-mode)
+      ;;
+      ;; We're in the agenda
+      ;;
+      (let* ((marker (org-get-at-bol 'org-hd-marker))
+             (tags (org-with-point-at marker (org-get-tags-at))))
+        (if (and (eq arg 4) tags)
+            (org-agenda-clock-in '(16))
+          (bh/clock-in-organization-task-as-default)))
+    ;;
+    ;; We are not in the agenda
+    ;;
+    (save-restriction
+      (widen)
+      ; Find the tags on the current task
+      (if (and (equal major-mode 'org-mode) (not (org-before-first-heading-p)) (eq arg 4))
+          (org-clock-in '(16))
+        (bh/clock-in-organization-task-as-default)))))
+
+(defun bh/punch-out ()
+  (interactive)
+  (setq bh/keep-clock-running nil)
+  (when (org-clock-is-active)
+    (org-clock-out))
+  (org-agenda-remove-restriction-lock))
+
+(defun bh/clock-in-default-task ()
+  (save-excursion
+    (org-with-point-at org-clock-default-task
+      (org-clock-in))))
+
+(defun bh/clock-in-parent-task ()
+  "Move point to the parent (project) task if any and clock in"
+  (let ((parent-task))
+    (save-excursion
+      (save-restriction
+        (widen)
+        (while (and (not parent-task) (org-up-heading-safe))
+          (when (member (nth 2 (org-heading-components)) org-todo-keywords-1)
+            (setq parent-task (point))))
+        (if parent-task
+            (org-with-point-at (or parent-task)
+              (org-clock-in))
+          (when bh/keep-clock-running
+            (bh/clock-in-default-task)))))))
+
+(defvar bh/organization-task-id "eb155a82-92b2-4f25-a3c6-0304591af2f9")
+
+(defun bh/clock-in-organization-task-as-default ()
+  (interactive)
+  (save-restriction
+    (widen)
+    (org-with-point-at (org-id-find bh/organization-task-id 'marker)
+      (org-clock-in '(16)))))
+
+(defun bh/clock-out-maybe ()
+  (when (and bh/keep-clock-running
+             (not org-clock-clocking-in)
+             (marker-buffer org-clock-default-task)
+             (not org-clock-resolving-clocks-due-to-idleness))
+    (bh/clock-in-parent-task)))
+
+(add-hook 'org-clock-out-hook 'bh/clock-out-maybe 'append)
