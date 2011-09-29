f477915130046e4e375cb8cecc526a1ef8f5ec32
tingle2008 <tingle2008@localhost>
2011-09-29 17:03:36 +0800

    autocomit

---
+++ ctips/pymako/gencode.py
@@ -0,0 +1,2 @@
+#!/home/tops/bin/python
+
---
+++ emacs/dotemacs.d/yasnippet-init.el
@@ -0,0 +1,4 @@
+(add-to-list 'load-path "~/.emacs.d/yasnippet-0.6.1c")
+(require 'yasnippet) ;; not yasnippet-bundle
+(yas/initialize)
+(yas/load-directory "~/.emacs.d/yasnippet-0.6.1c/snippets")
