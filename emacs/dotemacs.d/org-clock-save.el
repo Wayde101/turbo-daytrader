9dca13b44e69bbf85d7adec8e6d83a8178f02c93
tingle2008 <tingle2008@localhost>
2011-10-13 09:33:07 +0800

    autocomit

--- emacs/dotemacs.d/org-clock-save.el
+++ emacs/dotemacs.d/org-clock-save.el
@@ -1,7 +1,2 @@
-<<<<<<< local
-;; org-persist.el - tingbox at <2011-10-11 Tue 18:16>
-(setq stored-clock-history '(("/home/yuting/org/gemstone.org" . 1341) ("/home/yuting/org/refile.org" . 1639) ("/home/yuting/org/todolist.org" . 30) ("/home/yuting/org/gemstone.org" . 866)))
-=======
-;; org-persist.el - tingbox at <2011-10-12 三 08:52>
-(setq stored-clock-history '(("/home/yuting/src/turbo-daytrader/org/tradeplan.org" . 1685) ("/home/yuting/src/turbo-daytrader/org/todolist.org" . 30)))
->>>>>>> other
+;; org-persist.el - tingbox at <2011-10-13 Thu 09:31>
+(setq stored-clock-history '(("/home/yuting/org/refile.org" . 1958) ("/home/yuting/org/refile.org" . 3930) ("/home/yuting/org/todolist.org" . 30) ("/home/yuting/org/gemstone.org" . 524)))
