fabf778c75899a8c47beb7587faf894a139cad14
tingle2008 <tingle2008@localhost>
2011-10-14 22:06:02 +0800

    autocomit

--- emacs/dotemacs.d/org-clock-save.el
+++ emacs/dotemacs.d/org-clock-save.el
@@ -1,2 +1,1 @@
-;; org-persist.el - tingbox at <2011-10-13 Thu 18:09>
-(setq stored-clock-history '(("/home/yuting/org/refile.org" . 1958) ("/home/yuting/org/refile.org" . 3930) ("/home/yuting/org/todolist.org" . 30) ("/home/yuting/org/gemstone.org" . 524) ))
+;; org-persist.el - tingbox at <2011-10-14 五 21:53>
