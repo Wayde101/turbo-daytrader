1c53b3e64b767da15bf7911b4993f7cedb82bbab
tingle2008 <tingle2008@localhost>
2011-11-01 18:40:26 +0800

    autocomit

--- emacs/dotemacs.d/org-clock-save.el
+++ emacs/dotemacs.d/org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-persist.el - tingbox at <2011-10-28 五 09:00>
-(setq stored-clock-history '(("/home/yuting/src/turbo-daytrader/org/learning-orgmode.org" . 1170) ("/home/yuting/src/turbo-daytrader/org/learning-orgmode.org" . 1471) ("/home/yuting/src/turbo-daytrader/org/refile.org" . 2385) ("/home/yuting/src/turbo-daytrader/org/todolist.org" . 30) ("/home/yuting/src/turbo-daytrader/org/refile.org" . 2576)))
+;; org-persist.el - tingbox at <2011-11-01 Tue 18:11>
+(setq stored-clock-history '(("/home/yuting/src/turbo-daytrader/org/learning-orgmode.org" . 1220) ("/home/yuting/src/turbo-daytrader/org/learning-orgmode.org" . 1521) ("/home/yuting/src/turbo-daytrader/org/refile.org" . 2385) ("/home/yuting/src/turbo-daytrader/org/todolist.org" . 30) ("/home/yuting/src/turbo-daytrader/org/refile.org" . 2576)))
