c47d3944f50cdeb952042258690618e449c424ec
tingle2008 <tingle2008@localhost>
2011-11-08 09:34:50 +0800

    autocomit

--- emacs/dotemacs.d/org-clock-save.el
+++ emacs/dotemacs.d/org-clock-save.el
@@ -1,7 +1,2 @@
-<<<<<<< local
-;; org-persist.el - tingbox at <2011-11-04 Fri 16:11>
-(setq stored-clock-history '(("/home/yuting/src/turbo-daytrader/org/learning-orgmode.org" . 1220) ("/home/yuting/src/turbo-daytrader/org/learning-orgmode.org" . 1521) ("/home/yuting/src/turbo-daytrader/org/refile.org" . 2385) ("/home/yuting/src/turbo-daytrader/org/todolist.org" . 30) ("/home/yuting/src/turbo-daytrader/org/refile.org" . 2576) ("/home/yuting/org/tradeplan.org" . 8079)))
-=======
-;; org-persist.el - tingbox at <2011-11-07 一 07:36>
-(setq stored-clock-history '(("/home/yuting/src/turbo-daytrader/org/learning-orgmode.org" . 1170) ("/home/yuting/src/turbo-daytrader/org/learning-orgmode.org" . 1471) ("/home/yuting/src/turbo-daytrader/org/refile.org" . 2385) ("/home/yuting/src/turbo-daytrader/org/todolist.org" . 30) ("/home/yuting/src/turbo-daytrader/org/refile.org" . 2576)))
->>>>>>> other
+;; org-persist.el - tingbox at <2011-11-07 Mon 18:20>
+(setq stored-clock-history '(("/home/yuting/org/tradeplan.org" . 8298)))
