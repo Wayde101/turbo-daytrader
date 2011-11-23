10a16169507debfbb1c8e4e73089f5c3b4258804
tingle2008 <tingle2008@localhost>
2011-11-23 19:28:12 +0800

    autocomit

--- emacs/dotemacs.d/org-clock-save.el
+++ emacs/dotemacs.d/org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-persist.el - tingbox at <2011-11-21 Mon 16:26>
+;; org-persist.el - tingbox at <2011-11-23 Wed 18:42>
 (setq stored-clock-history '(("/home/yuting/org/tradeplan.org" . 8233) ("/home/yuting/org/tradeplan.org" . 8506)))
