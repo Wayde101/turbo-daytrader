747402e8e00a1adc442e921fdccdd9be9398baa4
tingle2008 <tingle2008@localhost>
2012-04-28 13:47:05 +0800

    homedir changed

--- homebin/hdesk.h
+++ homebin/hdesk.h
@@ -3,4 +3,4 @@
 #console
 #exec rdesktop -g 1024x768 shake.corp.linuxnote.net:3366
 
-exec rdesktop -g 1024x768 www.17forex.com:55558
+rdesktop -g 1024x768 -a 16 -K -xl -r sound:local -T '$TITLE' www.17forex.com:55558
