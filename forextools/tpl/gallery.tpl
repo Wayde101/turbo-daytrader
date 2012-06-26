77162c1bf2a01c4c006c0a1aa7d197501675c1c0
tingle2008 <tingle2008@localhost>
2012-06-26 17:47:40 +0800

    ??????

---
+++ forextools/tpl/gallery.tpl
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>${title}</title>
+	<meta name="author" content="Ste Brennan - Code Computerlove - http://www.codecomputerlove.com/" />
+	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;" name="viewport" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<link href="styles.css" type="text/css" rel="stylesheet" />
+	
+	<link href="../photoswipe.css" type="text/css" rel="stylesheet" />
+	
+	<script type="text/javascript" src="../lib/klass.min.js"></script>
+	<script type="text/javascript" src="../code.photoswipe-3.0.5.min.js"></script>
+	
+	
+	<script type="text/javascript">
+		(function(window, PhotoSwipe){
+			document.addEventListener('DOMContentLoaded', function(){
+				var options = {},
+				instance = PhotoSwipe.attach( window.document.querySelectorAll('#Gallery a'), options );
+			
+			}, false);
+		}(window, window.Code.PhotoSwipe));
+	</script>
+	
+</head>
+<body>
+
+<div id="Header">
+	<a href="${site}"><img src="${sitelogo}" width="230" height="48" alt="Code Computerlove" /></a>
+</div>
+
+<div id="MainContent">
+	<div class="page-content">
+		<h1>${title}</h1>
+	</div>
+	
+	<ul id="Gallery" class="gallery">
+	${expand()}
+	</ul>
+</div>	
+</body>
+</html>
--- forextools/webgallery.py
+++ forextools/webgallery.py
@@ -1,35 +1,34 @@
 #!/home/tops/bin/python
 
 import os, sys ,re,string
-import process
 import configuration
 from mako.template import Template
 from currencychart import CurrencyChart
+from convert_wrapper import ConvertWrapper
 
 
 class WebGallery:
     def __init__(self, config = configuration.Configuration()):
+        self.ftpath    = '/home/yuting/project/turbo-daytrader/forextools'
+        self.gbasedir   = '%s/html' % self.ftpath
         self.wmap       = dict()
-        self.wpath      = config.webgallerypath
-        self.wfullpath  = "%s/images/full" % self.wpath
-        self.wthumbpath = "%s/images/thumb" % self.wpath
         self.t_row      = config.timeframe_row
         self.c_col      = config.currency_col
-        self.tpl_file   = config.webgallerytpl
-        self.out_file   = config.webgalleryout
         return
 
-
-    def update(self):
+    def init_gallery(self,gname):
+        ghome    = "%s/%s" % (self.gbasedir,gname)
+        if not os.path.exists(ghome):
+            os.makedirs("%s/images/full"  % ghome )
+            os.makedirs("%s/images/thumb" % ghome )
+    
+    def update(self,gname):
         for item in self.wmap.keys():
             cname,tf = item.split('_')
-            c = CurrencyChart(cname,tf)
-            cp_full_cmd  = "cp -avf %s %s/%s.gif" % (c.get_latest_gif(), self.wfullpath, self.wmap[item])
-            cp_thumb_cmd = "cp -avf %s %s/%s.gif" % (c.get_latest_gif(), self.wthumbpath, self.wmap[item])
-            p = process.Process(cp_full_cmd)
-            p.runInConsole()
-            p = process.Process(cp_thumb_cmd)
-            p.runInConsole()
+            c  = CurrencyChart(cname,tf)
+            cp = ConvertWrapper();
+            cp.convert_copy(c.get_latest_gif() , "%s/%s/images/full/%s.gif" % (self.gbasedir, gname , self.wmap[item]))
+            cp.convert_copy(c.get_latest_gif() , "%s/%s/images/thumb/%s.gif" % (self.gbasedir, gname , self.wmap[item]))
         return
 
     def expand(self):
@@ -42,37 +41,52 @@ class WebGallery:
 
         return OUT_str
 
+
     def build(self,**kargs):
-        idx = 1
+        idx   = 1
+        g_title = kargs['title'] if kargs.has_key('title') else 'notitle'
+        
         if kargs.has_key('t_row'):
             self.t_row = kargs['t_row']
         if kargs.has_key('c_col'):
             self.c_col = kargs['c_col']
-        if kargs.has_key('htmlfile'):
-            self.out_file = kargs['htmlfile']
+            
+        gallery_tpl      = "%s/tpl/gallery.tpl" % self.ftpath
+        gallery_file     = "%s/%s/%s.html" % (self.gbasedir,g_title,g_title)
+        styles_css_tpl   = "%s/tpl/styles.css.tpl"% self.ftpath
+        styles_css_file  = "%s/%s/styles.css" % (self.gbasedir,g_title)
+
+        rpercent  = float(100) / float(len(self.t_row))
 
         for c in self.c_col:
             for t in self.t_row:
                 self.wmap[c + '_' + t] = '%0.3d' % idx
                 idx = idx + 1
-
-        self.update()
+        self.init_gallery(g_title)
+        self.update(g_title)
 
         context = dict()
-        context['title']    = kargs['title'] if kargs.has_key('title') else 'ForexGallary'
+        context['title']    = kargs['title'] if kargs.has_key('title') else 'notitle'
         context['site']     = 'http://www.17forex.com'
-        context['sitelogo'] =  'http://www.17forex.com/bbs/images/default/logo.gif'
+        context['sitelogo'] = 'http://www.17forex.com/bbs/images/default/logo.gif'
+        context['rpercent'] = "%.5f%%" % rpercent
         context['expand'] = self.expand
-        t = Template(filename = self.tpl_file)
-        f = open(self.out_file,'w')
-        f.write(t.render(**context))
-        f.close()
+        gt = Template(filename = gallery_tpl)
+        g = open(gallery_file,'w')
+        g.write(gt.render(**context))
+        g.close()
+        
+        st = Template(filename = styles_css_tpl)
+        s = open(styles_css_file,'w')
+        s.write(st.render(**context))
+        s.close()
 
 
 if __name__ == '__main__':
     g = WebGallery()
-    #g.update()
-    g.build(htmlfile='/home/yuting/src/3.0.5/examples/g.out.html',title='ForexGallary',t_row=['60','240','1440'],c_col=['eurusd','gbpusd'])
+    g.build(title='ForexGallery_1h',t_row=['60','240','1440','10080','43200'],c_col=['eurusd','gbpusd','usdchf','audusd','usdcad','usdjpy'])
+    g.build(title='ForexGallery_5m',t_row=['5','15','60','240'],c_col=['eurusd','gbpusd','usdchf','audusd','usdcad','usdjpy'])
+    g.build(title='juehai_5m',t_row=['5','15','60'],c_col=['eurusd','gbpusd','usdchf','audusd','usdcad','usdjpy'])
         
 
     
