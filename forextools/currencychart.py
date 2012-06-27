902063b3a4ba8b2a53d9896cd2a1e7dddcd05624
tingle2008 <tingle2008@localhost>
2012-06-27 17:37:16 +0800

    .

--- forextools/currencychart.py
+++ forextools/currencychart.py
@@ -4,13 +4,12 @@ import os, sys ,re,string
 from glob import glob
 import configuration
 
-
-
 class CurrencyChart:
     def __init__(self, cur_name,cur_tf, config = configuration.Configuration()):
         self.cur_name      = cur_name.upper()
         self.tf            = cur_tf
         self.fullpath      = ''
+
         # some status keys define at below
 
         for path in config.mt4shotpath:
@@ -19,21 +18,34 @@ class CurrencyChart:
                 self.fullpath = p
                 print "Found:", self.fullpath
                 pass
-
         return
         
     def get_latest_gif(self):
         imgs = glob('%s/*.gif' % self.fullpath)
-        imgs.sort();
+        imgs.sort()
         return imgs[-1]
 
     def get_status(self):
         status = '%s/status.csv' % self.fullpath
         return status
 
-    def load_status(self):
-        #from juehai
-        return
+    def dump_status(self):
+        def splitLine(line):
+            items = map(lambda x: tuple(x.strip().split('=')),
+                        line.split(';'))
+            return dict(items)
+        
+        content = list()
+        items = dict()
+        
+        with open(self.get_status(), 'r') as f:
+            content = f.xreadlines()
+            content = map(lambda x: splitLine(x.strip()), content)
+            for line in content:
+                items[line['OBJNAME']] = line
+            f.seek(0)
+            f.close()
+        return items
 
     def get_status_a(self):
         return
@@ -43,6 +55,5 @@ class CurrencyChart:
 
 if __name__ == '__main__':
     c = CurrencyChart("EURUSD","60")
-    print c.get_latest_gif()
-    print c.get_status()
+    print c.dump_status()
 
