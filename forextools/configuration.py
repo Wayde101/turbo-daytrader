713115704b71c954211521726444889718f74b29
tingle2008 <tingle2008@localhost>
2012-06-29 12:02:36 +0800

    .

--- forextools/configuration.py
+++ forextools/configuration.py
@@ -4,8 +4,10 @@ class Configuration:
     mt4shotpath = [
         '/home/yuting/project/yuting/Alpari/experts/files/shots'
         ]
-    
+
+    imgbakuppath   = '/home/forexchart/'
     convertbin     = '/usr/bin/convert'
+    rsyncbin       = '/usr/bin/rsync'
     timeframe_col  = ['60','240','1440','10080','43200']
     currency_row   = ['eurusd','gbpusd','usdchf','audusd','usdcad','usdjpy']
     currency_flip  = ['usdcad','usdjpy','usdchf']
