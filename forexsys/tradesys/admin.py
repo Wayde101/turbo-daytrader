3dea4fe70191851a2e6c23258e21a06f9ba56042
tingle2008 <tingle2008@localhost>
2012-09-05 15:23:46 +0800

    ?? README.md, ?tradesys ?????????

--- forexsys/tradesys/admin.py
+++ forexsys/tradesys/admin.py
@@ -1,5 +1,5 @@
 from tradesys.models import Symbol
-from tradesys.models import TimeFrame
+from tradesys.models import TimeFrame,TradeFrame,TradePlanModel,TradePlanAction
 from tradesys.models import MarketDirect
 from django.contrib import admin
 
@@ -15,11 +15,26 @@ class TimeFrameAdmin(admin.ModelAdmin):
         (None,               {'fields': ['timeframe']}),
     ]
 
+class TradeFrameAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['tradeframe']}),
+    ]
+    
+class TradePlanModelAdmin(admin.ModelAdmin):
+    pass
+
+class TradePlanActionAdmin(admin.ModelAdmin):
+    pass
+    
+
 class MarketDirectAdmin(admin.ModelAdmin):
     pass
 
 
 admin.site.register(Symbol,SymbolAdmin)
 admin.site.register(TimeFrame,TimeFrameAdmin)
+admin.site.register(TradeFrame,TradeFrameAdmin)
+admin.site.register(TradePlanModel,TradePlanModelAdmin)
+admin.site.register(TradePlanAction,TradePlanActionAdmin)
 admin.site.register(MarketDirect,MarketDirectAdmin)
 
