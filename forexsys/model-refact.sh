ce16416fff81fcc8e591065a4e3c0aea683cb18c
tingle2008 <tingle2008@localhost>
2012-09-07 13:14:05 +0800

    TradePlanSumView ?????????

--- forexsys/model-refact.sh
+++ forexsys/model-refact.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+#rbackup
+python ./manage.py dumpdata > data/initial-all.json
+sleep 10
+
 rm -f forexsys.db
 python ./manage.py syncdb --noinput
 python ./manage.py loaddata data/initial-all.json
 
-#re-backup
-python ./manage.py dumpdata > data/initial-all.json
 
