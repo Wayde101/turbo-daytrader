7f03c884dad4509d8f42a95013afe7065709e040
tingle2008 <tingle2008@localhost>
2012-09-07 12:25:39 +0800

    sample model-refact.sh add

--- forexsys/model-refact.sh
+++ forexsys/model-refact.sh
@@ -2,10 +2,8 @@
 
 rm -f forexsys.db
 python ./manage.py syncdb --noinput
-python ./manage.py loaddata data/initial-auth.json
-python ./manage.py loaddata data/initial-tradesys.json
+python ./manage.py loaddata data/initial-all.json
 
 #re-backup
-python ./manage.py dumpdata accounts > data/initial-auth.json
-python ./manage.py dumpdata tradesys > data/initial-tradesys.json
+python ./manage.py dumpdata > data/initial-all.json
 
