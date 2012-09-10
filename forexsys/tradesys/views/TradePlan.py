94d2fc3292bdd80ace803cb01c49d2eb7f31823c
tingle208 <tingle208@localhost>
2012-09-10 08:34:45 +0800

    MarketOverView is going

--- forexsys/tradesys/views/TradePlan.py
+++ forexsys/tradesys/views/TradePlan.py
@@ -18,21 +18,34 @@ from tradesys.models import TradeFrame
 from tradesys.forms import TradeFrameForm
 from django.contrib.auth.models import User
 
-class TradePlanSumView(ListView):
+class MyTradePlanView(ListView):
     # form_class    = TradeFrameForm
     model         = TradePlanAction
     template_name = "tradesys/TradePlanSumView.html"
 
-class TradePlanTest(ListView):
+
+    def get_context_data(self, **kwargs):
+        context = super(MyTradePlanView, self).get_context_data(**kwargs)
+        context['tradeframe'] = TradeFrameForm().as_ul()
+        return context
 
 
+# class TradePlanTest(ListView):
+
+    
 class TimeFrameMetrics(CreateView):
     form_class = MarketDirectForm
     model      = MarketDirect
     template_name = "tradesys/market_overview.html"
     success_url = "/"
-    
+
+
+# @login_required
+# def tp_sum_view(request, *args, **kwargs):
+    # latest_tp_list = TradePlanAction.objects.all().order_by('-tradeplan__begin_time')[:10]
+    # return render_to_response('tradesys/TradePlanSumView.html', {
+            # 'latest_tp_list': latest_tp_list})
 
 create_view = login_required(TimeFrameMetrics.as_view())
-tp_sum_view = login_required(TradePlanSumView.as_view())
+tp_sum_view = login_required(MyTradePlanView.as_view())
 
