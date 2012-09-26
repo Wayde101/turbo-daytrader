b0787d1f445bd7b5c9edc5682b3fc8a4dfbfac21
tingle208 <tingle208@localhost>
2012-09-26 11:06:53 +0800

    ????????

--- forexsys/tradesys/views/TradePlan.py
+++ forexsys/tradesys/views/TradePlan.py
@@ -64,7 +64,11 @@ class MarketOview(CreateView):
             p.save()
             self.request.session['TradePlanModel_id'] = p.id
         else:
-            s_d_t_list = ['%s_%s_%s' % (x,y,z) for x in [self.request.session['tradetype']] for y in ['obj_dir','sub_dir'] for z in trade_frame_map(self.request.session['tradeframe'])]
+            # s_d_t : symbol_direct_timeframe
+            s_d_t_list = ['%s_%s_%s' % (s,d,t)
+                          for s in [self.request.session['tradetype']]
+                          for d in ['obj_dir','sub_dir']
+                          for t in trade_frame_map(self.request.session['tradeframe'])]
             
             self.request.session['TradePlanModel_id'] = latest_tradeplan[0].id
             
@@ -75,21 +79,18 @@ class MarketOview(CreateView):
                 for zk in ['obj_dir','sub_dir']:
                     for tm in trade_frame_map(self.request.session['tradeframe']):
                         s_d_t = '%s_%s_%s' % (self.request.session['tradetype'],zk,tm)
-                        if zk == 'obj_dir':
-                            try:
-                                self.request.session[s_d_t] = MarketDetailInfo.objects.get(symbol_name = self.request.session['tradetype'],
-                                                                                           timeframe   = tm,
-                                                                                           market_overview = latest_tradeplan[0].market_overview).obj_dir
-                            except MarketDetailInfo.DoesNotExist:
-                                self.request.session[s_d_t] = 'N/A'
-                        if zk == 'sub_dir':
-                            try:
-                                self.request.session[s_d_t] = MarketDetailInfo.objects.get(symbol_name = self.request.session['tradetype'],
-                                                                                           timeframe   = tm,
-                                                                                           market_overview = latest_tradeplan[0].market_overview).sub_dir
-                            except MarketDetailInfo.DoesNotExist:
-                                self.request.session[s_d_t] = 'N/A'
-            
+                        latest_tradeplan
+
+                        try:
+                            mdi_obj = MarketDetailInfo.objects.get(symbol_name = self.request.session['tradetype'],
+                                                                   timeframe   = tm,
+                                                                   market_overview = latest_tradeplan[0].market_overview)
+                        except MarketDetailInfo.DoesNotExist:
+                            self.request.session[s_d_t] = 'N/A'
+                            continue
+
+                        self.request.session[s_d_t] = getattr(mdi_obj,zk)
+
         return initial
         
     def get_context_data(self, **kwargs):
@@ -107,49 +108,91 @@ def save_market_overview(request):
     tp = TradePlanModel.objects.get(id = request.session['TradePlanModel_id'])
     if tp.market_overview  is None:
         mov_obj = MarketOverView(market_result = request.POST['market_result'],
-                                 pub_date = timezone.now())
-
+                                 pub_date      = timezone.now())
         mov_obj.save()
         tp.market_overview = mov_obj
         tp.save()
-        
+
+    else:
+        setattr(tp.market_overview,'market_result',request.POST['market_result'])
+        tp.market_overview.save()
+        setattr(tp,'plan_result',request.POST['plan_result'])
+        tp.save()
         
     for each_tf in dir_list:
         tf_item = each_tf.split('_')
-        sym_object = tf_item[0]
-        tf_object  = tf_item[3]
+        symbol_name = tf_item[0]
+        timeframe  = tf_item[3]
 
         try:
-            md = MarketDetailInfo.objects.get(symbol_name = sym_object,
-                                              timeframe = tf_object,
+            md = MarketDetailInfo.objects.get(symbol_name = symbol_name,
+                                              timeframe = timeframe,
                                               market_overview = tp.market_overview)
             
         except MarketDetailInfo.DoesNotExist:
-            if tf_item[1]  == 'obj':
-                md = MarketDetailInfo(symbol_name = sym_object,
-                                      timeframe   = tf_object,
-                                      obj_dir     = request.POST[each_tf],
-                                      market_overview = tp.market_overview
-                                  )
-            if tf_item[1]  == 'sub':
-                md = MarketDetailInfo(symbol_name = sym_object,
-                                  timeframe   = tf_object,
-                                  sub_dir     = request.POST[each_tf],
-                                  market_overview = tp.market_overview
-                                  )
+            md = MarketDetailInfo()
+            setattr(md,'symbol_name',symbol_name)
+            setattr(md,'timeframe', timeframe)
+            setattr(md,'%s_dir' % tf_item[1], request.POST[each_tf])
+            setattr(md,'market_overview', tp.market_overview)
+            md.save()
+            continue
 
-                md.save()
-                continue
-            
-        if tf_item[1]  == 'sub':
-            md.sub_dir = request.POST[each_tf]
-        if tf_item[1]  == 'obj':
-            md.obj_dir = request.POST[each_tf]
-
+        setattr(md,'%s_dir' % tf_item[1] , request.POST[each_tf])
         md.save()
 
+
 def save_market_diffview(request):
-    pass
+    dir_list = filter(lambda x: re.search('_dir_',x), request.POST.keys())
+    tp = TradePlanModel.objects.get(id = request.session['TradePlanModel_id'])
+
+    for x in ['b', 's']:
+        result = 'diff_%s_result' % x
+        diff_x_overview = 'diff_%s_overview' % x
+        t = 1 if x == 'b' else 0
+        
+        timeframe = trade_frame_map(request.session['tradeframe'])[t]
+
+        
+        if getattr(tp,diff_x_overview) is None:
+            mdv_obj = MarketOverView(market_result = request.POST[result],
+                                     pub_date = timezone.now())        
+            mdv_obj.save()
+            setattr(tp,diff_x_overview,mdv_obj)
+            tp.save()
+
+        else:
+            diff_x_overview_obj = getattr(tp,diff_x_overview)
+            setattr(diff_x_overview_obj,'market_result',request.POST[result])
+            
+
+    for each_dir in dir_list:
+        symbol,zk,x,timeframe = each_dir.split('_')
+        
+        if timeframe == trade_frame_map(request.session['tradeframe'])[1]:
+            x = 'b'
+            
+        if timeframe == trade_frame_map(request.session['tradeframe'])[0]:
+            x = 's'
+
+        diff_x_overview_obj = getattr(tp,'diff_%s_overview' % x)
+            
+        try:
+            md = MarketDetailInfo.objects.get(symbol_name = symbol,
+                                              timeframe = timeframe,
+                                              market_overview = diff_x_overview_obj)
+        except MarketDetailInfo.DoesNotExist:
+            md = MarketDetailInfo()
+            setattr(md,'symbol_name',symbol)
+            setattr(md,'timeframe',timeframe)
+            setattr(md,'market_overview',diff_x_overview_obj)
+            setattr(md,'%s_dir' % zk, request.POST[each_dir])
+            setattr(md,'normative', request.POST['%s_normative_%s' % (symbol,timeframe)])
+
+            if x == 's':
+                setattr(md,'strength',request.POST['%s_strength_%s' % (symbol,timeframe)])
+                
+            md.save()
 
 
 class MarketDview(CreateView):
@@ -166,27 +209,39 @@ class MarketDview(CreateView):
     def get_context_data(self, **kwargs):
         context = super(MarketDview, self).get_context_data(**kwargs)
         context['tradeframe'] = self.request.session['tradeframe']
+        # should be defind in models
         symbols = ['EURUSD','GBPUSD','CHFUSD','AUDUSD','CADUSD','JPYUSD']
         context['market_diff_view'] = MarketDiffViewForm(symbols,trade_frame_map(context['tradeframe'])).as_ul()
         return context
 
-
-class FirstSelectView(DetailView):
+class FirstSelectedView(CreateView):
     model  = MarketDetailInfo
     template_name = 'tradesys/FirstSelectView.html'
 
     def get_initial(self):
-        initial = super(FirstSelectView, self).get_initial()
+        initial = super(FirstSelectedView, self).get_initial()
         initial = initial.copy()
-
+        
         save_market_diffview(self.request)
+        
+        
         return initial
 
     def get_context_data(self, **kwargs):
-        context = super(FirstSelectView, self).get_context_data(**kwargs)
+        context = super(FirstSelectedView, self).get_context_data(**kwargs)
+
+        tp_obj  = TradePlanModel.objects.get(id = request.session['TradePlanModel_id'])
+        mdi_obj = MarketDetailInfo.objects.filter(market_overview = tp_obj.diff_s_overview.id)
+
+        # for mdi in mdi_obj:
+            
+        
+        # should be defind in models
+        symbols = ['EURUSD','GBPUSD','CHFUSD','AUDUSD','CADUSD','JPYUSD']
+        
         return context
 
 tp_sum_view       = login_required(MyTradePlanView.as_view())
 market_over_view  = login_required(MarketOview.as_view())
 market_diff_view  = login_required(MarketDview.as_view())
-first_select_view = login_required(FirstSelectView.as_view())
+first_select_view = login_required(FirstSelectedView.as_view())
