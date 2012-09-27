992240e46499d35c373a6f35809485b8c7f023c5
tingle208 <tingle208@localhost>
2012-09-27 09:56:36 +0800

    gogogo

--- forexsys/model-refact.sh
+++ forexsys/model-refact.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 #rbackup
-python ./manage.py dumpdata > data/initial-all.json
-sleep 10
+# python ./manage.py dumpdata > data/initial-all.json
+# sleep 10
 
 rm -f forexsys.db
 python ./manage.py syncdb --noinput
-python ./manage.py loaddata data/initial-all.json
+python ./manage.py loaddata data/initial-auth.json
 
 
