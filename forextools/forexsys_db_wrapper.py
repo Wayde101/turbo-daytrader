f272f0c4f1ba5e2a71926fd8ee80d0e630ff4ff1
tingle2008 <tingle2008@localhost>
2012-10-30 14:41:04 +0800

    ??????????

--- forextools/forexsys_db_wrapper.py
+++ forextools/forexsys_db_wrapper.py
@@ -5,18 +5,30 @@ import logging
 import log
 import process
 import configuration
+import time
 
 class ForexsysDb:
     def __init__(self, config = configuration.Configuration()):
         self.pg_dump  = config.pg_dump
+        self.pg_backup   = config.pg_backup
+        
+        if not os.path.isdir(self.pg_backup):
+            os.mkdir(self.pg_backup)
         
 
-    def backup(self, dst):
-        bak_cmd = '%s -f %s' % (self.pg_dump,dst)
+    def backup(self):
+        tm_year,tm_mon,tm_mday,tm_hour,tm_min,tm_sec,tm_wday,tm_yday,tm_isdst=time.localtime()
+        bak_file = '%s%s%s.dump' % (tm_year, 
+                               tm_mon,
+                               tm_mday)
+
+        bak_cmd  = '%s -f %s/%s' % (self.pg_dump,
+                                    self.pg_backup,
+                                    bak_file)
 
         p = process.Process(bak_cmd)
         p.runInConsole()
         return
-        
 
 
+# vim: ts=4 sw=4 ai et
