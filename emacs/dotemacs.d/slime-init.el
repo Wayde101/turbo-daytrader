57e256cc4d8a1d44d5c44da2788a69b732d917b3
tingle2008 <tingle2008@localhost>
2012-11-05 13:33:55 +0800

    merged

--- emacs/dotemacs.d/slime-init.el
+++ emacs/dotemacs.d/slime-init.el
@@ -5,7 +5,7 @@
 (add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
 (add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
 ;; Optionally, specify the lisp program you are using. Default is "lisp"
-(setq inferior-lisp-program "/usr/local/bin/sbcl") 
+(setq inferior-lisp-program "/usr/bin/sbcl") 
 (slime-setup '(slime-fancy slime-asdf))
 
 ;; (add-to-list 'load-path "~/.emacs.d/slime/")  ; your SLIME directory
